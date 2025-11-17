0b314897a1b92de502a1ec73dcf7172a86da47d0
GigaHierz <lenahierzi@hotmail.de>
2025-11-17 10:41:23 -0300

    Fix spelling mistake in BuenoToken

--- contracts/BuenoToken.sol
+++ contracts/BuenoToken.sol
@@ -5,7 +5,7 @@ import {ERC20} from "@openzeppelin/contracts@5.4.0/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts@5.4.0/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable} from "@openzeppelin/contracts@5.4.0/access/Ownable.sol";
 
-contract Buen0Token is ERC20, Ownable, ERC20Permit {
+contract BuenoToken is ERC20, Ownable, ERC20Permit {
     constructor(address initialOwner)
         ERC20("BuenoToken", "BTK")
         Ownable(initialOwner)
