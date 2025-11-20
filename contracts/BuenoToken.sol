4d6710c47448d9492361d88a70b643d495969d11
ryestew <rob@stupay.com>
2025-11-20 03:23:33 -0300

    update token and remove deps

--- contracts/BuenoToken.sol
+++ contracts/BuenoToken.sol
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity 0.8.27;
 
 import {ERC20} from "@openzeppelin/contracts@5.4.0/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "@openzeppelin/contracts@5.4.0/token/ERC20/extensions/ERC20Permit.sol";
-import {Ownable} from "@openzeppelin/contracts@5.4.0/access/Ownable.sol";
+import "@openzeppelin/contracts@5.4.0/access/AccessControl.sol";
 
-contract BuenoToken is ERC20, Ownable, ERC20Permit {
-    constructor(address initialOwner)
+contract BuenoToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor()
         ERC20("BuenoToken", "BTK")
-        Ownable(initialOwner)
-        ERC20Permit("BuenoToken")
-    {}
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    {
+    	_grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    	_grantRole(MINTER_ROLE, msg.sender);        
     }
+
+	function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+    	_mint(to, amount);
+	}
+
+	function decimals() public pure override returns (uint8) {
+    	return 2;
+	} 
 }
